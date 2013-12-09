14f7aa5e521499d2ec7f4b362f55278cd137413d
Greg Molnar <molnargerg@gmail.com>
2013-12-09 13:59:32 +0100

    initial commit

---
+++ lib/rack/metrics.rb
@@ -0,0 +1,7 @@
+require "rack/metrics/version"
+
+module Rack
+  module Metrics
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/metrics/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Metrics
+    VERSION = "0.0.1"
+  end
+end
