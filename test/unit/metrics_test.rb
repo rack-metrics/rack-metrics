7673663265cc7be50060d0e30f8c13e4a687ed09
Greg Molnar <molnargerg@gmail.com>
2014-02-21 09:19:16 +0100

    fix travis

--- test/unit/metrics_test.rb
+++ test/unit/metrics_test.rb
@@ -12,18 +12,18 @@ class MetricsTest < ActiveSupport::TestCase
 
 	test "it subscribes to sql.active_record" do
 		ActiveSupport::Notifications.instrument('sql.active_record'){}
-		refute_empty Rack::Metrics.current.template.queries
+		assert !Rack::Metrics.current.template.queries.empty?
 	end
 
 	test "it subscribes to render_template.action_view" do
 		ActiveSupport::Notifications.instrument('render_template.action_view'){}
-		refute_nil Rack::Metrics.current.template
+		assert !Rack::Metrics.current.template.nil?
 	end
 
 	test "it subscribes to render_partial.action_view" do
 		ActiveSupport::Notifications.instrument('start_render_partial.action_view'){}
 		ActiveSupport::Notifications.instrument('render_partial.action_view'){}
-		refute_empty Rack::Metrics.current.template.partials
+		assert !Rack::Metrics.current.template.partials.empty?
 	end
 
 	test "it nests render_partial.action_view" do
@@ -35,9 +35,9 @@ class MetricsTest < ActiveSupport::TestCase
 					ActiveSupport::Notifications.instrument('render_partial.action_view'){}
 			end
 		end
-		refute_empty Rack::Metrics.current.template.partials
+		assert !Rack::Metrics.current.template.partials.empty?
 		assert_equal 1, Rack::Metrics.current.template.partials.count
-		refute_empty Rack::Metrics.current.template.partials[0].partials
+		assert !Rack::Metrics.current.template.partials[0].partials.empty?
 	end
 
 	test "it adds query to nested render_partial.action_view" do
@@ -48,7 +48,7 @@ class MetricsTest < ActiveSupport::TestCase
 				ActiveSupport::Notifications.instrument('render_partial.action_view') do
 			end
 		end
-		refute_empty Rack::Metrics.current.template.partials
+		assert !Rack::Metrics.current.template.partials.empty?
 		assert_equal 1, Rack::Metrics.current.template.partials.count
 		assert_equal 0, Rack::Metrics.current.template.queries.count
 		assert_equal 1, Rack::Metrics.current.template.partials[0].partials[0].queries.count
